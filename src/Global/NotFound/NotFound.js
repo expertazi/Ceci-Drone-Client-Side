72938503d19b4a63e29b28b302ffad070f9621e7
Md Azizul <banglatechmaster1@gmail.com>
2021-11-12 03:04:22 +0600

    404 page added

--- src/Global/NotFound/NotFound.js
+++ src/Global/NotFound/NotFound.js
@@ -1,9 +1,30 @@
 import React from "react";
+import { Col, Container, Row, Button } from "react-bootstrap";
+import { Link } from "react-router-dom";
 
 const NotFound = () => {
   return (
-    <div>
-      <h2>Not Found</h2>
+    <div className="my-5">
+      <Container>
+        <Row>
+          <Col md={4}></Col>
+          <Col md={4}>
+            {" "}
+            <img
+              className="img-fluid"
+              src="https://cdn.dribbble.com/users/1138875/screenshots/4669703/404_animation.gif"
+              alt=""
+            />
+            <h2>Page Not Found</h2>
+            <Link to="/home">
+              <button className="btn btn-global-color mb-5 mt-3">
+                Go Back Home
+              </button>
+            </Link>
+          </Col>
+          <Col md={4}></Col>
+        </Row>
+      </Container>
     </div>
   );
 };
